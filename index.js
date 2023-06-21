4ec78c9abb8b11e812c4d77cb171a12e5b4d5812
dinesh16a <dineshbabu.t2021csea@sece.ac.in>
2023-06-21 11:04:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+OBJECT
+const user={
+    name:'xzy',
+    age: 25
+};
+console.log(user)
+
+const user={
+    name:'xzy',
+    age: 25
+};
+user.schoole='din'//adding element in object
+console.log(user)
+delete user.age;  //delect inside
